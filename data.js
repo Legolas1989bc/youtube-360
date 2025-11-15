1b9ecd1daca09dde1dd497ed966d0945c332c803
Legolas1989bc <81646429+Legolas1989bc@users.noreply.github.com>
2025-11-15 16:13:01 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,190 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama1",
+      "name": "Panorama1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.05041926803996155,
+        "pitch": 0.061660615373813954,
+        "fov": 1.3991932276324333
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6486917001182384,
+          "pitch": 0.14669386398000483,
+          "rotation": 0,
+          "target": "1-panorama2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.061035627088458,
+          "pitch": 0.1979519645525034,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Sedia Poliform</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">lorem ipsum</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama2",
+      "name": "Panorama2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2859939013634563,
+          "pitch": 0.1376148148572458,
+          "rotation": 0,
+          "target": "0-panorama1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1354903056111887,
+          "pitch": 0.1347912458132523,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">tavolo x y z</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">bdvsbbdsvjvdjdk</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-panorama3",
+      "name": "Panorama3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3535362101058972,
+          "pitch": 0.09732337456131646,
+          "rotation": 0,
+          "target": "3-panorama4"
+        },
+        {
+          "yaw": 1.888999187126693,
+          "pitch": 0.2932562179664018,
+          "rotation": 0,
+          "target": "1-panorama2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama4",
+      "name": "Panorama4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07505953344886862,
+          "pitch": 0.07284811973076444,
+          "rotation": 0,
+          "target": "2-panorama3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9041654102326788,
+          "pitch": 0.1687927777078304,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">piano cottura all in one</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">bhdsb&lt;bfsf&lt;</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
